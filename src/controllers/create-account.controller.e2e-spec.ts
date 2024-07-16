24f07c3dadbbf5aca8df4ff9c371830575aaf36e
Victor Canto <iamvictorcanto@gmail.com>
2024-07-16 03:04:51 -0300

    test: ensure account creation is working

---
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -0,0 +1,37 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '@/app.module';
+import request from 'supertest';
+import { PrismaService } from '@/prisma/prisma.service';
+
+describe('Create account (e2e)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    prisma = moduleFixture.get<PrismaService>(PrismaService);
+    await app.init();
+  });
+
+  it('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(response.status).toBe(201);
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: 'johndoe@example.com',
+      },
+    });
+    expect(userOnDatabase).toBeTruthy();
+  });
+});
