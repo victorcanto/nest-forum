09df7522d00d2060800a57ef46ecca86206f2a7f
Victor Canto <iamvictorcanto@gmail.com>
2024-07-20 16:23:48 -0300

    feat: make routes private by default and add env module

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,19 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { Env } from '@/infra/env';
 import { JwtStrategy } from './jwt.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { EnvService } from '@/infra/env/env.service';
+import { EnvModule } from '@/infra/env/env.module';
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
-      inject: [ConfigService],
+      imports : [EnvModule],
+      inject: [EnvService],
       global: true,
-      useFactory: (config: ConfigService<Env, true>) => {
-        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true });
-        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
+      useFactory: (env: EnvService) => {
+        const privateKey = env.get('JWT_PRIVATE_KEY');
+        const publicKey = env.get('JWT_PUBLIC_KEY');
         return {
           privateKey: Buffer.from(privateKey, 'base64'),
           publicKey: Buffer.from(publicKey, 'base64'),
@@ -25,6 +28,10 @@ import { JwtStrategy } from './jwt.strategy';
       },
     }),
   ],
-  providers: [JwtStrategy],
+  providers: [
+    EnvService,
+    JwtStrategy,
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+  ],
 })
 export class AuthModule {}
