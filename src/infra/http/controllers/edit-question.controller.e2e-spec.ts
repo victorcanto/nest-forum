a3a14f623f4f98d83a602e6692b0a70a445a204d
Victor Canto <iamvictorcanto@gmail.com>
2024-07-29 16:05:50 -0300

    test: ensure the question can be edited

---
+++ src/infra/http/controllers/edit-question.controller.e2e-spec.ts
@@ -0,0 +1,61 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '@/infra/app.module';
+import request from 'supertest';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { DatabaseModule } from '@/infra/database/database.module';
+import { StudentFactory } from '@/test/factories/make-student';
+import { QuestionFactory } from '@/test/factories/make-question';
+
+describe('Edit question (e2e)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let questionFactory: QuestionFactory;
+  let studentFactory: StudentFactory;
+  let jwt: JwtService;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    prisma = moduleFixture.get<PrismaService>(PrismaService);
+    studentFactory = moduleFixture.get<StudentFactory>(StudentFactory);
+    questionFactory = moduleFixture.get<QuestionFactory>(QuestionFactory);
+    jwt = moduleFixture.get(JwtService);
+    await app.init();
+  });
+
+  it('[PUT] /questions/:id', async () => {
+    const user = await studentFactory.makePrismaStudent();
+
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    });
+
+    const questionId = question.id.toString();
+
+    const response = await request(app.getHttpServer())
+      .put(`/questions/${questionId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: 'New title',
+        content: 'New content',
+      });
+
+    expect(response.status).toBe(204);
+
+    const questionUpdatedOnDatabase = await prisma.question.findFirst({
+      where: {
+        title: 'New title',
+        content: 'New content',
+      },
+    });
+    expect(questionUpdatedOnDatabase).toBeTruthy();
+  });
+});
