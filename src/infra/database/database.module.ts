8001382c9e260bcc3e02ca5fd141638954a976b7
Victor Canto <iamvictorcanto@gmail.com>
2024-08-14 12:38:56 -0300

    feat: implement service, repository and cache module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -18,8 +18,10 @@ import { NotificationsRepository } from '@/domain/notification/application/repos
 import { PrismaNotificationsRepository } from './prisma/repositories/prisma-notifications-repository';
 import { AttachmentsRepository } from '@/domain/forum/application/repositories/attachments-repository';
 import { PrismaAttachmentsRepository } from './prisma/repositories/prisma-attachments-repository';
+import { CacheModule } from '../cache/cache.module';
 
 @Module({
+  imports: [CacheModule],
   providers: [
     PrismaService,
     {
